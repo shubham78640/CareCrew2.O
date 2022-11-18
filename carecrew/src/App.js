9d4c106c45a1ab54e4e87b2753a80448b27327c7
theintezar1 <108331681+theintezar1@users.noreply.github.com>
2022-11-18 11:41:47 +0530

    header

--- carecrew/src/App.js
+++ carecrew/src/App.js
@@ -1,8 +1,22 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Navbaar from './components/Headers/Navbaar'
+import Home from './components/Home/Home'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+      <Navbaar/>
+        <Box>
+          <Routes>
+              <Route path="/" element={<Home/>} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </div>
+
   )
 }
 
---
+++ carecrew/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeCrousel from '../Crousel/HomeCrousel'
+
+function Home() {
+  return (
+    <div>
+        <HomeCrousel/>
+    </div>
+  )
+}
+
+export default Home
