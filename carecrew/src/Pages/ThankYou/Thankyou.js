8a5c08232c0c295279930ac4629496bd05e076f8
theintezar1 <108331681+theintezar1@users.noreply.github.com>
2023-02-07 18:17:08 +0530

    thank you

--- carecrew/src/Pages/ThankYou/Thankyou.js
+++ carecrew/src/Pages/ThankYou/Thankyou.js
@@ -8,7 +8,7 @@ function Thankyou() {
   return (
     <>
     <Box  sx={{width:{sm:"50%", xs:"100%"}, height:"100vh", display:"flex", alignItems:"center", justifyContent:"center", margin:"auto"}}>
-    <img width="100%" src={thankyou} alt="" />
+    <img width="100%" src="https://astrixinc.com/wp-content/uploads/2017/05/thank-you.png" alt="" />
     </Box>
     <Button onClick={()=>{nevigate("/")}} color="success" sx={{position:"relative", left:{sm:"45%", xs:"32%"}, bottom:{sm:"200px", xs:"200px"}}} variant='contained' >Go to homepage</Button>
     </>
