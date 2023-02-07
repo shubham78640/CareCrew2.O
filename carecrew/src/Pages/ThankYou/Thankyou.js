4bf6c1c3ac3ab4f021b7829ab15aac4cdd5ddaa9
theintezar1 <108331681+theintezar1@users.noreply.github.com>
2023-02-07 18:14:22 +0530

    change thankyou image

--- carecrew/src/Pages/ThankYou/Thankyou.js
+++ carecrew/src/Pages/ThankYou/Thankyou.js
@@ -1,16 +1,16 @@
 import { Box, Button } from '@mui/material'
 import React from 'react'
-import thankyou from "../../Images/thankyou.webp"
+import thankyou from "../../Images/Thank.png"
 import { useNavigate } from 'react-router-dom'
 
 function Thankyou() {
     let nevigate = useNavigate()
   return (
     <>
-    <Box border={1} sx={{width:{sm:"50%", xs:"100%"}, height:"100vh", display:"flex", alignItems:"center", justifyContent:"center", margin:"auto"}}>
-    <img width="100%" src={thankyou} alt="" />
+    <Box sx={{width:{sm:"50%", xs:"100%"}, height:"100vh", display:"flex", alignItems:"center", justifyContent:"center", margin:"auto"}}>
+    <img width="100%" src="https://astrixinc.com/wp-content/uploads/2017/05/thank-you.png" alt="" />
     </Box>
-    <Button onClick={()=>{nevigate("/")}} color="success" sx={{position:"relative", left:{sm:"45%", xs:"32%"}, bottom:{sm:"270px", xs:"380px"}}} variant='contained' >Go to homepage</Button>
+    <Button onClick={()=>{nevigate("/")}} color="success" sx={{position:"relative", left:{sm:"45%", xs:"32%"}, bottom:{sm:"200px", xs:"200px"}}} variant='contained' >Go to homepage</Button>
     </>
   )
 }
