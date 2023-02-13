5223dfa691be45e7ceaff05f72307113356a983d
theintezar1 <108331681+theintezar1@users.noreply.github.com>
2023-02-13 17:55:48 +0530

    add e-mail

--- carecrew/src/Pages/JobSection/JobsHindi.js
+++ carecrew/src/Pages/JobSection/JobsHindi.js
@@ -67,7 +67,7 @@ function JobsHindi() {
       console.log(response);
       const config = {
         SecureToken: "64bbee42-d25a-4fff-ad6d-5133e8409c45",
-        To: "shubham@thepinchlife.com",
+        To: "everyhome@carecrew.in",
         From: "intezar@thepinchlife.com",
         Subject: "Help Form Lead",
         Body: `<p>Name:- ${name}</p> 
