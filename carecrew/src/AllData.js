fed0cd76056220021be84aca4b1830306d6b0c85
shubham78640 <shubham@thepinchlife.com>
2023-05-08 13:05:57 +0530

    updated google sheet  Api

--- carecrew/src/AllData.js
+++ carecrew/src/AllData.js
@@ -140,6 +140,7 @@ export let CardsData = [
 
 ]
 
-export const MasterApi ="https://sheetdb.io/api/v1/htskf8c4cu3gx"
+// export const MasterApi ="https://sheetdb.io/api/v1/htskf8c4cu3gx"
+export const MasterApi ="https://sheetdb.io/api/v1/nepz7t98wbmy1"
 
 export const MasterApi1 ="https://sheetdb.io/api/v1/qk6b271zq8g0z"
